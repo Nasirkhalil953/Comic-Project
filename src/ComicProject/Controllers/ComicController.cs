0f114256de876d493a3db836032b0282757ab73f
Nasir <Nasir@DESKTOP-A2FLCEO>
2018-11-04 19:42:53 +0500

    Update Comic Detail View.Setting of Data into the Controller

--- src/ComicProject/Controllers/ComicController.cs
+++ src/ComicProject/Controllers/ComicController.cs
@@ -10,10 +10,22 @@ namespace ComicProject.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            //{
+            //    return Redirect("/");
+            //}
+            ViewBag.SeriesTitle = "The Amazing Spider Man";
+            ViewBag.ItemNumber = 700;
+            ViewBag.Description = "<p>this si the paragraph which I wrote first time during...<strong>programing of ASP.NET</strong></p>";
+            ViewBag.Authers = new string[]
             {
-                return Redirect("/");
-            }
+                "script : ABC",
+                "Pencil : GHI",
+                "Ink color : JKL",
+                "Writer : MNO"
+            };
+
+
             return View();
         }
     }
