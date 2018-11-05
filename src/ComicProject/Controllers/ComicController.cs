40ef2c193f11dc4f13bcfb60520f1f707e5d93e9
Nasir <Nasir@DESKTOP-A2FLCEO>
2018-11-05 12:39:35 +0500

    Updated Detail View to be strongly typed

--- src/ComicProject/Controllers/ComicController.cs
+++ src/ComicProject/Controllers/ComicController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicProject.Models;
 
 namespace ComicProject.Controllers
 {
@@ -10,27 +11,40 @@ namespace ComicProject.Controllers
     {
         public ActionResult Detail()
         {
+            var comic = new Comic()
+            {
+                SeriesTitle = "The Amazing Spider Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Based on<Strong> Marvel Comics'</Strong> superhero character," +
+                                  " this is a story of <Strong>Peter Parker</Strong> who is a nerdy high school." +
+                                  " He was orphaned as a child, bullied by jocks, and can't confess " +
+                                  "his crush for his stunning neighborhood girl <Strong>Mary Jane Watson</Strong>." +
+                                  " To say his life is is an understatement</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "script",Role = "Alpha"},
+                    new Artist() {Name = "Pencil",Role = "HB Products"},
+                    new Artist() {Name = "Ink color",Role = "eown Library"},
+                    new Artist() {Name = "Writer",Role = "Nasir Khalil"}
+                }
+
+            };
             //if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
             //{
             //    return Redirect("/");
             //}
-            ViewBag.SeriesTitle = "The Amazing Spider Man";
-            ViewBag.ItemNumber = 700;
-            ViewBag.Description = "<p>Based on<Strong> Marvel Comics'</Strong> superhero character," +
-                                  " this is a story of <Strong>Peter Parker</Strong> who is a nerdy high school." +
-                                  " He was orphaned as a child, bullied by jocks, and can't confess " +
-                                  "his crush for his stunning neighborhood girl <Strong>Mary Jane Watson</Strong>." +
-                                  " To say his life is is an understatement</p>";
-            ViewBag.Authers = new string[]
-            {
-                "script : Alpha",
-                "Pencil : HB Products",
-                "Ink color : Jeown Library",
-                "Writer : Nasir Khalil"
-            };
+            //ViewBag.SeriesTitle = "";
+            //ViewBag.ItemNumber = ;
+            //ViewBag.Description = ";
+            //ViewBag.Authers = new string[]
+            //{
+            //    "script : Alpha",
+            //    "Pencil : HB Products",
+            //    "Ink color : Jeown Library",
+            //    "Writer : Nasir Khalil"
+            //};
 
-
-            return View();
+            return View(comic);
         }
     }
 }
--- src/ComicProject/Models/Comic.cs
+++ src/ComicProject/Models/Comic.cs
@@ -10,7 +10,7 @@ namespace ComicProject.Models
         public int Id { set; get; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string Description { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorites { get; set; }
 
