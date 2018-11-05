01eddabe1744b7041bf907e33c00c10be6eeb67a
Nasir <Nasir@DESKTOP-A2FLCEO>
2018-11-05 09:59:26 +0500

    Added Comic and Artist Model

---
+++ src/ComicProject/Models/Comic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.`;
+using System.Linq;
+using System.Web;
+
+namespace ComicProject.Models
+{
+    public class Comic
+    {
+        public int Id { set; get; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorites { get; set; }
+    }
+}
