57c02ec4c1a0c6c4546bd1e6f8d36b5911fe2b28
Nasir <Nasir@DESKTOP-A2FLCEO>
2018-11-11 11:47:02 +0500

    Update the Default Route

--- src/ComicProject/App_Start/RouteConfig.cs
+++ src/ComicProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ComicProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Comic", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
